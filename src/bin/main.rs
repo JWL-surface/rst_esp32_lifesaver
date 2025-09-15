84b071eaf4e8da9b77a2eee42d7da68ac0b73a3f
Taimur Khan <taimurkhan@microsoft.com>
2025-09-15 14:47:13 -0700

    wifi not working with embassy

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,13 +6,17 @@
     holding buffers for the duration of a data transfer."
 )]
 
+use embassy_executor::Spawner;
 use esp_hal::clock::CpuClock;
 use esp_hal::gpio::{Level, Output, OutputConfig};
 use esp_hal::main;
 use esp_println::println;
 use esp_hal::time::{Duration, Instant};
 use esp_hal::timer::timg::TimerGroup;
+use esp_hal::timer::systimer::SystemTimer;
 use esp_wifi::wifi::{self, WifiController};
+use static_cell::StaticCell;
+
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -28,35 +32,45 @@ esp_bootloader_esp_idf::esp_app_desc!();
 const SSID: &str = "Iphone";
 const PASSWORD: &str = "12345678";
 
-#[main]
-fn main() -> ! {
+#[esp_hal_embassy::main]
+async fn main(spawner: Spawner) {
     // generator version: 0.5.0
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
     esp_alloc::heap_allocator!(size: 64 * 1024);
 
-    let mut led = Output::new(peripherals.GPIO21, Level::High, OutputConfig::default());
-    let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let init = esp_wifi::init(timg0.timer0, esp_hal::rng::Rng::new(peripherals.RNG)).unwrap();
+    let timer0 = SystemTimer::new(peripherals.SYSTIMER);
+    esp_hal_embassy::init(timer0.alarm0);
 
-    let (mut controller, interfaces) = esp_wifi::wifi::new(&init, peripherals.WIFI).unwrap();
+    let mut led = Output::new(peripherals.GPIO21, Level::High, OutputConfig::default());
+
+    static WIFI_INIT_CELL: StaticCell<esp_wifi::EspWifiController> = StaticCell::new();
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let wifi_init = WIFI_INIT_CELL.init(esp_wifi::init(timg0.timer0, esp_hal::rng::Rng::new(peripherals.RNG)).unwrap());
+
+    let (mut controller, interfaces) = esp_wifi::wifi::new(wifi_init, peripherals.WIFI).unwrap();
     let mut device = interfaces.sta;
 
-    // config wifi
+    spawner.spawn(connect_wifi(controller)).unwrap();
+    spawner.spawn(blink_led(led)).unwrap();
+}
+
+#[embassy_executor::task]
+async fn connect_wifi(mut controller: WifiController<'static>) {
     let client_config = wifi::Configuration::Client(wifi::ClientConfiguration {
-    ssid: SSID.try_into().unwrap(),
-    password: PASSWORD.try_into().unwrap(),
-    ..Default::default()
+        ssid: SSID.try_into().unwrap(),
+        password: PASSWORD.try_into().unwrap(),
+        ..Default::default()
     });
+
     let res = controller.set_configuration(&client_config);
     println!("Wifi config: {:?}", res);
 
-    // Start the wifi controller
     controller.start().unwrap();
 
     let c = controller.connect();
-    println!("wifi_connect {:?}", c);
+    println!("Wifi connect {:?}", c);
 
     println!("Waiting to connect...");
     loop {
@@ -67,12 +81,14 @@ fn main() -> ! {
         }
     }
     println!("Connected!");
+}
 
+#[embassy_executor::task]
+async fn blink_led(mut led: Output<'static>) {
     loop {
+        println!("Toggle");
         led.toggle();
-        let delay_start = Instant::now();
+        let delay_start: Instant = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.0/examples/src/bin
 }
