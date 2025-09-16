5e902b07733a1e06a1c48dc1e5d1d041dc7c0024
Taimur Khan <taimurkhan@microsoft.com>
2025-09-16 11:28:29 -0700

    working tcp

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,23 +6,20 @@
     holding buffers for the duration of a data transfer."
 )]
 
-use core::str::SplitWhitespace;
-
+use blocking_network_stack::Stack;
 use embassy_executor::Spawner;
 use embedded_io::{Read, Write};
 use esp_hal::clock::CpuClock;
 use esp_hal::gpio::{Level, Output, OutputConfig};
-use esp_hal::main;
 use esp_println::println;
 use esp_hal::time::{Duration, Instant};
 use esp_hal::timer::timg::TimerGroup;
 use esp_hal::timer::systimer::SystemTimer;
-use esp_wifi::wifi::{self, WifiController};
-use heapless::vec;
+use esp_wifi::wifi::{self, WifiController, WifiDevice};
 use static_cell::StaticCell;
 use smoltcp::iface::{Config, Interface, SocketSet, SocketStorage};
-use smoltcp::wire::{DhcpOption, EthernetAddress, IpCidr, Ipv4Address, TcpPacket};
-use smoltcp::socket::tcp;
+use smoltcp::wire::{DhcpOption, IpAddress};
+use smoltcp::socket::{self, dhcpv4, tcp};
 use smoltcp::time::Instant as SmolInstant;
 
 #[panic_handler]
@@ -48,6 +45,7 @@ async fn main(spawner: Spawner) {
     esp_alloc::heap_allocator!(size: 64 * 1024);
 
     let timer0 = SystemTimer::new(peripherals.SYSTIMER);
+    let mut rng = esp_hal::rng::Rng::new(peripherals.RNG);
     esp_hal_embassy::init(timer0.alarm0);
 
     let mut led = Output::new(peripherals.GPIO21, Level::High, OutputConfig::default());
@@ -55,48 +53,47 @@ async fn main(spawner: Spawner) {
     // wifi setup
     static WIFI_INIT_CELL: StaticCell<esp_wifi::EspWifiController> = StaticCell::new();
     let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let wifi_init = WIFI_INIT_CELL.init(esp_wifi::init(timg0.timer0, esp_hal::rng::Rng::new(peripherals.RNG)).unwrap());
+    // let wifi_init = WIFI_INIT_CELL.init(esp_wifi::init(timg0.timer0, esp_hal::rng::Rng::new(peripherals.RNG)).unwrap());
+
+    let wifi_init = WIFI_INIT_CELL.init(esp_wifi::init(timg0.timer0, rng.clone()).unwrap());
 
     let (mut controller, interfaces) = esp_wifi::wifi::new(wifi_init, peripherals.WIFI).unwrap();
     let mut device = interfaces.sta;
 
     // Network setup
-    let m = smoltcp::wire::HardwareAddress::Ethernet(EthernetAddress([0x02,0x00,0x00,0x12,0x34,0x56]));
-    let mut config = Config::new(m);
-    let mut iface = Interface::new(config, &mut device, SmolInstant::from_millis(0));
-
-    // i have no idea
-    static mut SOCKETS_STORAGE: StaticCell<[SocketStorage<'static>; 4]> = StaticCell::new();
-    unsafe {
-        let mut sockets_store = SOCKETS_STORAGE.init([SocketStorage::EMPTY, SocketStorage::EMPTY, SocketStorage::EMPTY, SocketStorage::EMPTY]);
-        let mut sockets = SocketSet::new(&mut sockets_store[..]);
-    }
+    let addr = smoltcp::wire::HardwareAddress::Ethernet(smoltcp::wire::EthernetAddress::from_bytes(&device.mac_address()));
+    let mut config_dev = Config::new(addr);
+    let mut iface = Interface::new(config_dev, &mut device, SmolInstant::from_millis(0));
     
-    spawner.spawn(token)
+    // i have no idea
+    static SOCKETS_STORAGE: StaticCell<[SocketStorage<'static>; 3]> = StaticCell::new();
+    let socket_set_entries = SOCKETS_STORAGE.init([SocketStorage::EMPTY, SocketStorage::EMPTY, SocketStorage::EMPTY]).as_mut();
+    let mut socket_set = SocketSet::new(&mut socket_set_entries[..]);
+    let mut dhcp_socket = smoltcp::socket::dhcpv4::Socket::new();
 
-    // let mut dhcp_socket = smoltcp::socket::dhcpv4::Socket::new();
+    // we can set a hostname here (or add other DHCP options)
+    dhcp_socket.set_outgoing_options(&[DhcpOption {
+        kind: 12,
+        data: b"implRust",
+    }]);
+    socket_set.add(dhcp_socket);
 
-    // dhcp_socket.set_outgoing_options(&[DhcpOption {
-    //     kind: 12,
-    //     data: b"implRust",
-    // }]);
-    // socket_set.add(dhcp_socket);
+    let now = || esp_hal::time::Instant::now().duration_since_epoch().as_millis();
+    let mut stack = Stack::new(
+        iface,
+        device,
+        socket_set,
+        now,
+        rng.random(),
+    );
 
-    spawner.spawn(connect_wifi(controller)).unwrap();
+    spawner.spawn(connect_wifi(controller, stack)).unwrap();
     spawner.spawn(blink_led(led)).unwrap();
-
-    loop {
-        let now = SmolInstant::from_millis(embassy_time::Instant::now().as_millis() as i64);
-        match iface.poll(now, &mut device, &mut sockets) {
-            smoltcp::iface::PollResult::SocketStateChanged => {println!("SocketStateChanged");}
-            smoltcp::iface::PollResult::None => { println!("None"); }
-        }
-        embassy_time::Timer::after_millis(100).await;
-    }
+    // let _ = spawner.spawn(net_task(iface, device, sockets));
 }
 
 #[embassy_executor::task]
-async fn connect_wifi(mut controller: WifiController<'static>) {
+async fn connect_wifi(mut controller: WifiController<'static>, mut stack: Stack<'static, WifiDevice<'static>>) {
     let client_config = wifi::Configuration::Client(wifi::ClientConfiguration {
         ssid: SSID.try_into().unwrap(),
         password: PASSWORD.try_into().unwrap(),
@@ -121,8 +118,34 @@ async fn connect_wifi(mut controller: WifiController<'static>) {
     }
     println!("Connected!");
 
+    println!("Wait for IP address");
     loop {
-        embassy_time::Timer::after_secs(1).await;
+        stack.work();
+        if stack.is_iface_up() {
+            println!("IP acquired: {:?}", stack.get_ip_info());
+            break;
+        }
+    }
+
+    static RX_BUFF: StaticCell<[u8; 1536]> = StaticCell::new();
+    static TX_BUFF: StaticCell<[u8; 1536]> = StaticCell::new();
+    let mut rx_buffer: &'static mut [u8; 1536] = RX_BUFF.init([0;1536]);
+    let mut tx_buffer: &'static mut [u8; 1536] = TX_BUFF.init([0;1536]);
+    let mut socket = stack.get_socket(rx_buffer, tx_buffer);
+
+    println!("Opening socket");
+    socket.work();
+    
+    let remote_addr = IpAddress::v4(172, 20, 10, 7);
+    socket.open(remote_addr, 8080).unwrap();
+
+    loop {
+        socket
+            .write(b"Hello from ESP32\r\n")
+            .unwrap();
+        socket.flush().unwrap();
+
+        embassy_time::Timer::after_secs(2).await;
     }
 }
 
@@ -133,15 +156,4 @@ async fn blink_led(mut led: Output<'static>) {
         led.toggle();
         embassy_time::Timer::after_secs(1).await;
     }
-}
-
-#[embassy_executor::task]
-async fn net_task(mut iface: Interface, mut sockets: SocketSet<'static>) {
-    let mut rx_buff = [0u8; 1024];
-    let tcp_rx_buff = tcp::SocketBuffer::new(&mut rx_buff[..]);
-    let mut tx_buff = [0u8; 1024];
-    let tcp_tx_buff = tcp::SocketBuffer::new(&mut tx_buff[..]);
-
-    let tcp_socket = tcp::Socket::new(tcp_rx_buff, tcp_tx_buff);
-    let handle = sockets.add(tcp_socket);
 }
